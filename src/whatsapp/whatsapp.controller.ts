05734a2f29c9b8d4425a38d21d97382f0413b4dd
warundev <120333797+warundev@users.noreply.github.com>
2025-05-18 18:58:03 +0530

    Subscribe the meta webhook

--- src/whatsapp/whatsapp.controller.ts
+++ src/whatsapp/whatsapp.controller.ts
@@ -1,10 +1,28 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Req, Res } from '@nestjs/common';
+import { Request, Response } from 'express';
 
 @Controller('whatsapp')
 export class WhatsappController {
-    @Get("test")
-    test()
-    {
-        return "test whatsapp message";
+  @Get('test')
+  test() {
+    return "warun's test whatsapp message";
+  }
+
+  @Get('webhook')
+  challangewebhook(@Req() req: Request, @Res() res: Response) {
+    const mode = req.query['hub.mode'];
+    const token = req.query['hub.verify_token'];
+    const challenge = req.query['hub.challenge'];
+
+    if (mode && token) {
+      if (mode === 'subscribe' && token === '123456789') {
+        console.log('WEBHOOK_VERIFIED');
+        res.status(200).send(challenge);
+      } else {
+        res.sendStatus(403);
+      }
+    } else {
+      res.sendStatus(400); // Optional: Bad Request if required params missing
     }
+  }
 }
