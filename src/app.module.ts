440349ebb296fcfe11499fa336ef415e0cbc6336
warundev <120333797+warundev@users.noreply.github.com>
2025-05-19 13:40:23 +0530

    Implement WhatsApp message sending and improve webhook handling

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { WhatsappController } from './whatsapp/whatsapp.controller';
+import { config } from 'process';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [ConfigModule.forRoot()],
   controllers: [AppController, WhatsappController],
   providers: [AppService],
 })
