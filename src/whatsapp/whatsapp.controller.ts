93b2c484d8cd5ea0e43ae9f35a91b49eae9146c1
warundev <120333797+warundev@users.noreply.github.com>
2025-05-19 13:40:02 +0530

    Implement WhatsApp message sending and improve webhook handling

--- src/whatsapp/whatsapp.controller.ts
+++ src/whatsapp/whatsapp.controller.ts
@@ -15,7 +15,7 @@ export class WhatsappController {
     const challenge = req.query['hub.challenge'];
 
     if (mode && token) {
-      if (mode === 'subscribe' && token === '123456789') {
+      if (mode === 'subscribe' && token === process.env.WHATSAPP_CHALLANGE_KEY) {
         console.log('WEBHOOK_VERIFIED');
         res.status(200).send(challenge);
       } else {
@@ -27,11 +27,68 @@ export class WhatsappController {
   }
 
   @Post('webhook')
-  handlewebhook(@Req() req: Request, @Res() res: Response) {
-    console.log('===== INCOMING WHATSAPP WEBHOOK =====');
-    console.log('Headers:', req.headers);
-    console.log('Body:', JSON.stringify(req.body, null, 2));
-    console.log('======================================');
-    res.sendStatus(200);
+handlewebhook(@Req() req: Request, @Res() res: Response) {
+  const body = req.body;
+
+  if (
+    body.object === 'whatsapp_business_account' &&
+    body.entry?.[0]?.changes?.[0]?.value?.messages
+  ) {
+    const value = body.entry[0].changes[0].value;
+
+    const message = value.messages[0];
+    const contact = value.contacts[0];
+
+    const phoneNumber = contact.wa_id;             // Sender's number
+    const senderName = contact.profile.name;       // Sender's name
+    const messageBody = message.text?.body;        // Message text (check optional chaining)
+
+    console.log('=============================');
+    console.log('📩 Incoming WhatsApp Message:');
+    console.log(`👤 Name: ${senderName}`);
+    console.log(`📞 Phone: ${phoneNumber}`);
+    console.log(`💬 Message: ${messageBody}`);
+    console.log('=============================');
+
+    this.sendmessage(phoneNumber, "hello from nestjs");
   }
+
+  res.sendStatus(200);
+}
+
+
+
+  async sendmessage(to:string, message:string) {
+    const axios = require('axios');
+let data = JSON.stringify({
+  "messaging_product": "whatsapp",
+  "recipient_type": "individual",
+  "to": to,
+  "type": "text",
+  "text": {
+    "preview_url": false,
+    "body": message
+  }
+});
+
+let config = {
+  method: 'post',
+  maxBodyLength: Infinity,
+  url: `https://graph.facebook.com/${process.env.WHATSAPP_API_VERSION}/${process.env.WHATSAPP_PHONE_NUMBER_ID}/messages`,
+  headers: { 
+    'Content-Type': 'application/json',
+    'Authorization': `Bearer ${process.env.WHATSAPP_API_KEY}`
+  },
+  data : data
+};
+
+axios.request(config)
+.then((response) => {
+  console.log(JSON.stringify(response.data));
+})
+.catch((error) => {
+  console.log(error);
+});
+
+}
 }
